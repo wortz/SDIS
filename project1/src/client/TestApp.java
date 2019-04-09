504de7569c7eb25f6eeaadbf837595b6564e49e2
alvarofranciscomiranda <up201603694@fe.up.pt>
2019-04-09 14:53:39 +0100

    TestApp updated

--- project1/src/client/TestApp.java
+++ project1/src/client/TestApp.java
@@ -4,41 +4,74 @@ import rmi.RmiInterface;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-public class TestApp{
-    private int peer_ap;
+public class TestApp implements Runnable {
+    private String peer_ap;
     private String sub_protocol;
     private String opnd_1;
-    private String opnd_2;
+    private int opnd_2;
 
-    public TestApp(int peer_ap, String sub_protocol, String opnd_1, String opnd_2) {
+    private RmiInterface stub;
+
+
+    public TestApp(String peer_ap, String sub_protocol, String opnd_1, int opnd_2) {
         this.peer_ap = peer_ap;
         this.sub_protocol = sub_protocol;
         this.opnd_1 = opnd_1;
         this.opnd_2 = opnd_2;
+
+        initiateRMIStub();
+
+        switch (this.sub_protocol){
+            case "backup"
+                break;
+            case "restore"
+                break;
+            case "delete"
+                break;
+            case "reclaim"
+                break;
+            case "state"
+                break;
+
+        }
     }
 
 
         public static void main(String args[]){
 
-            if(args.length<4){
+            if(args.length < 2 args.length > 4){
                 System.out.println("Error: The format must be : java TestApp <peer_ap> <sub_protocol> <opnd_1> <opnd_2>");
                 return;
             }
 
-            String host = null;
-            String protocol=args[1];
-            String file=args[2];
-            int rep_deg=Integer.parseInt(args[3]);
+            String peer_ap = args [0];
+            String protocol = args[1];
 
-            try {
-                Registry registry = LocateRegistry.getRegistry(host);
+            String opnd_1 = null;
+            if(args.length > 2)
+                opnd_1 = args[2];
 
-                RmiInterface stub = (RmiInterface) registry.lookup("remote");
-                stub.backupFile("penguin.gif", 1);
-                System.out.println("response");
-            } catch (Exception e) {
-                System.err.println("Client exception: " + e.toString());
-                e.printStackTrace();
+            String opnd_2 = null;
+            if(args.length > 3)
+                String opnd_1 = Integer.parseInt(args[3]);
+
+            TestApp application = new TestApp(peer_ap, protocol, opnd_1, opnd_2);
+            new Thread(application).start();
+
+
+
+    }
+
+    private void initiateRMIStub() {
+        try {
+            Registry registry = getRegistry(peer_ap);
+            this.stub = (RmiInterface) registry.lookup(this.sub_protocol);
+
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
         }
     }
+
+
 }
