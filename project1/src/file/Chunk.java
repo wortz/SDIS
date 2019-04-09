841544b0d290e7f83e2ba605304cb272007c5fd3
alvarofranciscomiranda <up201603694@fe.up.pt>
2019-04-09 17:32:07 +0100

    Chunk class initializer

--- project1/src/file/Chunk.java
+++ project1/src/file/Chunk.java
@@ -1,2 +1,39 @@
 package file;
 
+public class Chunk{
+    private int index;
+    private byte[] data;
+    private int replicationDegree;
+    private String fileID;
+    private int senderID;
+
+    public Chunk(int index, byte[] data, int replicationDegree, String fileID, int senderID) {
+        this.index = chunkIndex;
+        this.data = data;
+        this.replicationDegree = replicationDegree;
+        this.fileID = fileID;
+        this.senderID = senderID;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public int getReplicationDegree() {
+        return replicationDegree;
+    }
+
+    public String getFileID() {
+        return fileID;
+    }
+
+    public int getSenderID() {
+        return senderID;
+    }
+
+
+}
